cba2aef56e6cdc3fc5f51d4c5306ca8b0a723e41
DanielePiccioni <danielepiccioni1@gmail.com>
2025-10-16 13:47:33 +0200

    tools- logic to start wrtite the code in js

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,13 @@ Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente de
 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
+
+/* 
+TOOLS:
+-mostrare 5 numeri casuali
+-parte un timer di 30 secondi
+-spariscono i numeri e appaiono 5 input
+-l'utente deve inserire i 5 numeri visti prima
+-verificare se i numeri inseriti sono corretti
+-stamparlo in pagina
+*/
